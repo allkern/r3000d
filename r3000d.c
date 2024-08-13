e9aa71e8062bf87ea67730c90001e82817dfe7b2
allkern <lisandroaalarcon@gmail.com>
2024-08-13 17:15:14 -0300

    Add additional register names

--- r3000d.c
+++ r3000d.c
@@ -119,7 +119,39 @@ static const char* r3000_cop0_register_names[] = {
     "SR",
     "Cause",
     "EPC",
-    "PRId"
+    "PRId",
+    "r16",
+    "r17",
+    "r18",
+    "r19",
+    "r20",
+    "r21",
+    "r22",
+    "r23",
+    "r24",
+    "r25",
+    "r26",
+    "r27",
+    "r28",
+    "r29",
+    "r30",
+    "r31",
+    "r32",
+    "r33",
+    "r34",
+    "r35",
+    "r36",
+    "r37",
+    "r38",
+    "r39",
+    "r40",
+    "r41",
+    "r42",
+    "r43",
+    "r44",
+    "r45",
+    "r46",
+    "r47"
 };
 
 char* disassemble_secondary(char* buf, uint32_t opcode, r3000d_state* state) {
@@ -347,9 +379,6 @@ char* r3000d_disassemble(char* buf, uint32_t opcode, r3000d_state* state) {
             sprintf(ptr, "<invalid>");
         break;
     }
-
-    if (state)
-        state->addr += 4;
 
     return buf;
 }
